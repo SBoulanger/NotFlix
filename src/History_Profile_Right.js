e88659bce607d228fd1d5e2f979487299fecd18a
Isabelle Delmas <isabelle.lenertz@gmail.com>
2019-03-23 22:34:09 -0700

    added History_Profile_Right.js

---
+++ src/History_Profile_Right.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class History_Profile_Right extends React.Component {
+  render() {
+    return (
+      <div className="profile">
+        <button>History</button>
+        <br />
+        <button>My Favorits</button>
+        <br />
+        <button>My Friends</button>
+        <br />
+        <button>Logout</button>
+      </div>
+    );
+  }
+}
+
+export default History_Profile_Right
