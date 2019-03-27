c1b0c0e001bb2e43877f79da911449ee1349d38a
Timothy To <43382722+tdtimothy@users.noreply.github.com>
2019-03-27 01:25:37 -0700

    Updating test case

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import HistoryProfile from './History_Profile';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Movie_Page from "./Movie_Page";
 
 //You can change this render function to check on your components
 //Change HistoryProfile to YourAppFile
-ReactDOM.render(<HistoryProfile />, document.getElementById('root'));
-
+ReactDOM.render(<Movie_Page />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
