b617268df7eabb74803b63b6392397a00194f557
Anne Suzuki <anne.sa.suzuki@gmail.com>
2019-04-06 04:44:05 -0300

    New member sign up page.
    JS component for a new member sign up page. Does not have firestore functionalities.

---
+++ src/NewSignUp.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Notflix from './img/Notflix.png';
+import Button from './Button.js';
+import InputTextBox from './InputTextBox.js';
+import LinkText from './LinkText.js';
+
+class NewSignUp extends Component {
+   render() {
+      return (
+         <div className = "NewSignUp">
+            <header className = "NewSignUp-header">
+               <img src = {Notflix} className = "Notflix-logo" style = {{width: '30%'}} />
+            </header>
+            <body>
+              <p> <InputTextBox promptText="Email here" type = "text" id = "EmailBox" /> </p> 
+              <p> <InputTextBox promptText="Password here" type = "password" id = "PasswordBox" /> </p>
+              <p> <InputTextBox promptText="Confirm password" type = "password" id = "ConfirmPassword" /> </p>
+              <p> <Button text="Sign Up"/> </p>
+              <p> <LinkText pathway="https://www.google.com" label="Already a member?" /> </p>
+            </body>
+          </div>
+      );
+   }
+}
+
+export default NewSignUp;
+
