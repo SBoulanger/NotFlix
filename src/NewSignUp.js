363189729e479cdbe2f128044ded92dba993364a
Anne Suzuki <anne.sa.suzuki@gmail.com>
2019-04-08 15:23:33 -0700

    Added some firestore functions
    Adds new user (hard-coded) to firestore database on submit.

--- src/NewSignUp.js
+++ src/NewSignUp.js
@@ -4,8 +4,30 @@ import Notflix from './imgs/Notflix.png';
 import Button from './Button.js';
 import InputTextBox from './InputTextBox.js';
 import LinkText from './LinkText.js';
+import firebase from './Firestore';
 
 class NewSignUp extends Component {
+   
+   constructor() {
+      super();
+      this.state = {
+         email: "test@test.com",
+         password:"test",
+         confirmation:"test
+      }
+      this.addUser = this.addUser.bind(this);
+   }
+   
+   addUser = e = > {
+      e.preventDefault();
+      const db = firebase.firestore();
+      db.settings({ timestampsInSnapshots: true });
+      const userRef = db.collection("Users").add({
+         email: this.state.email,
+         password: this.state.password
+      });
+   }
+   
    render() {
       return (
          <div className = "NewSignUp">
@@ -13,11 +35,13 @@ class NewSignUp extends Component {
                <img src = {Notflix} className = "Notflix-logo" style = {{width: '30%'}} />
             </header>
             <body>
+            <form onSubmit = {this.addUser}>
               <p> <InputTextBox promptText="Email here" type = "text" id = "EmailBox" /> </p> 
               <p> <InputTextBox promptText="Password here" type = "password" id = "PasswordBox" /> </p>
               <p> <InputTextBox promptText="Confirm password" type = "password" id = "ConfirmPassword" /> </p>
               <p> <Button text="Sign Up"/> </p>
               <p> <LinkText pathway="https://www.google.com" label="Already a member?" /> </p>
+             </form>
             </body>
           </div>
       );
