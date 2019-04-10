6c9916361dc92ec92119f60bc827f2d074fbfe23
Anne Suzuki <anne.sa.suzuki@gmail.com>
2019-04-10 11:06:55 -0700

    Added name and function as passable properties to InputTextBox
    Can add name / function to InputTextBox via <InputTextBox name = {name here} onchange = {function here}/>

--- src/InputTextBox.js
+++ src/InputTextBox.js
@@ -6,12 +6,14 @@ class InputTextBox extends Component {
                 super(props);
                 this.promptText = this.promptText;
                 this.type = this.type;
-                this.id = this.id;
+                this.id = this.id; {/*id for CSS*/}
+                this.onchange = this.onchange; {/*pass in function*/}
+                this.name = this.name; {/*name to get value from component*/}
         }
 
         render() {
                 return (
-                        <input type = {this.props.type} placeholder = {this.props.promptText}/>
+                        <input type = {this.props.type} onChange={this.props.onchange} name={this.props.name} placeholder = {this.props.promptText}/>
                 );
         }
 }
