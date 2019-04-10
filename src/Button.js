5eca2fe00a3c8ad5545c04f005db50d5af035a97
Anne Suzuki <anne.sa.suzuki@gmail.com>
2019-04-10 10:59:33 -0700

    Added function property to Button.js
    Can pass functions to Button component via <Button onClick={this.function}/>

--- src/Button.js
+++ src/Button.js
@@ -5,12 +5,13 @@ class Button extends Component {
 
         constructor(props) {
                 super(props);
-                this.text = this.text;
+                this.text = this.text; {/*displayable text*/}
+                this.onClick = this.onClick; {/*stores function when clicked*/}
         }
 
         render() {
                 return (
-                     <button className = "Button">{this.props.text}</button>
+                     <button className = "Button" onClick = {this.props.onClick}>{this.props.text}</button>
                 );
         }
 }
