a8b60315591e564b9900d1cd6c5cf8005d14eae4
Kayla Walton <kayla.walton@gmail.com>
2019-04-12 12:29:31 -0700

    Small changes to some css files, working new signup

--- src/NewSignUp.js
+++ src/NewSignUp.js
@@ -8,24 +8,25 @@ import SignIn from './SignIn.js';
 import './NewSignUp.css';
 
 class NewSignUp extends Component {
-   
+
    constructor() {
       super();
       this.state = {
          email: '',
          password:'',
-         confirmation:''
+         confirmation:'Hi',
+         username:''
       }
       this.updateInput = this.updateInput.bind(this);
       this.addUser = this.addUser.bind(this);
    }
-   
+
    /* Re-renders page using SignIn.js component */
    toSignIn(){
       ReactDOM.render(<SignIn/>, document.getElementById('root'));
    }
 
-   /* NOT WORKING: Intended to update state to whatever is in InputTextBox */
+   /* WORKING: Intended to update state to whatever is in InputTextBox */
    updateInput = e => {
       this.setState({
          [e.target.name]: e.target.value
@@ -38,47 +39,56 @@ class NewSignUp extends Component {
 
    /* INCOMPLETE
    Intended actions:
-      1) make new document in Firestore with new credentials  
+      1) make new document in Firestore with new credentials - WORKING
       2) generate cookie
       3) redirect to user's home page
    */
-   addUser = e = > {
+   addUser = e => {
       e.preventDefault();
       /*INCOMPLETE: Add check so that password and confirmation states are same string.
-      If password == confirmation, proceed below. Else, show user that there is a mistake 
+      If password == confirmation, proceed below. Else, show user that there is a mistake
       or a user with those credentials doesn't exist.
       */
       const db = firebase.firestore();
       db.settings({ timestampsInSnapshots: true });
-      const userRef = db.collection("Users").doc(); /* makes new doc in Firestore with automatic ID*/
+      const userRef = db.collection("users").doc(); /* makes new doc in Firestore with automatic ID*/
       const ID = userRef.id; /* automatic ID stored as string in ID */
       userRef.set({
          autoID : ID,  /* WORKING: able to get autoID and store as string */
          email: this.state.email, /*NOT WORKING*/
-         password: this.state.password /*NOT WORKING*/
+         password: this.state.password, /*NOT WORKING*/
+         username: this.state.username
       });
-      /*INCOMPLETE: Generate cookie with auto-ID as credentials. Rerender using cookie to 
+      /*INCOMPLETE: Generate cookie with auto-ID as credentials. Rerender using cookie to
       user home page.*/
    }
-   
+
    /*Initial render*/
-   render() {
-      return (
-         <div className = "NewSignUp">
-            <header className = "NewSignUp-header">
-               <img src = {Notflix} className = "Notflix-logo" style = {{width: '30%'}} />
-            <form onSubmit = {this.addUser}>
-              <p> <InputTextBox promptText="Email here" type = "text" id = "EmailBox" name = "email" onchange = {this.updateInput} value = {this.state.email} /> </p> 
-              <p> <InputTextBox promptText="Password here" type = "password" id = "PasswordBox" name = "password" onchange = {this.updateInput} value = {this.state.password} /> </p>
-              <p> <InputTextBox promptText="Confirm password" type = "password" id = "ConfirmPassword" / name = "confirmation" onchange = {this.updateInput} value = {this.state.confirmation}> </p>
-              <p> <Button type="Submit" text="Sign Up"/> </p>
-              <p style = {{cursor: 'pointer'}} onClick {this.toSignIn}> Already a member? </p>
-             </form>
-            </header>
-          </div>
-      );
-   }
+
+      render() {
+        return (
+          <form onSubmit={this.addUser}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={this.updateInput}
+              value={this.state.username}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={this.updateInput}
+              value={this.state.email}
+            />
+            <button type="submit">
+            Submit
+            </button>
+          </form>
+        );
+      }
+
 }
 
 export default NewSignUp;
-
