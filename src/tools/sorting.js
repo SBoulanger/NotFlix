caf409d9160c469403b3b761824f127c2bd1d678
Samuel Boulanger <samuelrdboulanger@gmail.com>
2019-04-13 13:08:15 -0700

    Create tools directory for all tool related functions; created definition of sorting function

---
+++ src/tools/sorting.js
@@ -0,0 +1,18 @@
+//Author : Samuel Boulanger
+//Date   : 13 April 2019
+//
+
+
+//General Sorting function
+/*
+ *  Input  ( 
+ *          key     : what attribute to sort by
+ *          list    : what we are sorting
+ *          inverse : [True if inverse] sort in
+ *                      normal or inverse order 
+ *         )
+ *  Output ( list  : the sorted list )
+ */
+function sort(key, list, inverse) {
+
+}
