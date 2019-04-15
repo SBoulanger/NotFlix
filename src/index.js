0f5cd93fe63d77e4df52b3b9405742860978ee2e
Matthew-Ruben <36095451+Matthew-Ruben@users.noreply.github.com>
2019-04-15 07:59:28 -0700

    Added navbar to home page

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import HistoryPage from './History_Page';
 import * as serviceWorker from './serviceWorker';
 import Movie_Page from "./Movie_Page";
 import SignIn from "./SignIn"
+import NavBar from "./NavBar";
 
 //You can change this render function to check on your components
 //Change HistoryProfile to YourAppFile
 
+ReactDOM.render(<NavBar />, document.getElementById('navbar'))
 ReactDOM.render(<SignIn />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
