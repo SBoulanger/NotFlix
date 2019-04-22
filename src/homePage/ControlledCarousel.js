221e2acf5602177eb8f1a88f045050e71a82a5dd
elizabeth-wong-ab <ewong586@gmail.com>
2019-04-22 11:18:47 -0700

    Aligned buttons and made css more consistent with sign-in/signup

---
+++ src/homePage/ControlledCarousel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Carousel from 'nuka-carousel';
+
+export default class extends React.Component {
+    render() {
+        return (
+
+            <Carousel>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcyj8UkU4lpTmO5lzarKgoKIovetWrKkicUOkB1p28qj0N5swjxw" height="400px" width="0px"/>
+            </Carousel>
+
+        );
+    }
+}
