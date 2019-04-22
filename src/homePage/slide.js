cc2ce685462481cfc4f5e93cac617d23d0c8008c
elizabeth-wong-ab <ewong586@gmail.com>
2019-04-22 11:14:15 -0700

    Aligned buttons and made css more consistent with sign-in/signup

---
+++ src/homePage/slide.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Slider = ({image, width, height}) => {
+    const backgroundImage = '';
+    const styles = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center top',
+        backgroundRepeat: 'no-repeat',
+        float: 'left',
+        width: `${width}px`,
+        height: `${height}px`
+    }
+    return (
+        <div className="slide" style={styles}></div>
+    )
+}
+
+Slider.defaultProps = {
+    width: 300,
+    height: 600
+}
+
+Slider.propTypes = {
+    image: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+}
+
+export default Slider;
---
+++ src/homePage/slider.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Slide from './slide'
+import './slider.css'
+
+export default class Slider extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            width: 300,
+            height: 800,
+            images: ['nature-1.jpg', 'nature-2.jpg', 'nature-3.jpg', 'nature-4.jpg', 'nature-5.jpg', 'nature-6.jpg']
+        }
+    }
+
+    slideLeft() {
+        let last = this.state.images.slice(-1)
+        let rest = this.state.images.slice(0, -1)
+        let images = [last, ...rest]
+        this.setState({images: images});
+    }
+
+    slideRight() {
+        let [first, ...rest] = this.state.images;
+        let images = [...rest, first];
+        this.setState({images: images});
+    }
+
+    renderNavigation() {
+        return (
+            <div className="slider-arrows">
+                <a className="arrow left" onClick={() => this.slideLeft()}>
+                    <img src='http://icons.iconarchive.com/icons/iconsmind/outline/512/Arrow-Left-icon.png' />
+
+                </a>
+                <a className="arrow right" onClick={() => this.slideRight()}>
+                    <img src='http://icons.iconarchive.com/icons/iconsmind/outline/512/Arrow-Right-icon.png' />
+
+                </a>
+            </div>
+        )
+    }
+
+    renderSlides() {
+        const images = this.state.images;
+        return (
+            <div className="slider-items">
+                {
+                    images.map((image, index) => {
+                        return (
+                            <Slide image={image} width={this.state.width} height={this.state.height} key={index} />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div className="slider">
+                {this.renderNavigation()}
+                {this.renderSlides()}
+            </div>
+        )
+    }
+}
