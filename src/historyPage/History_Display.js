a67a5916eee89450fa8929cb6b30a80a0543fa40
Brian Lai <brianlai@MacBook-Pro.local>
2019-04-21 19:04:31 -0700

    fixed files

--- src/historyPage/History_Display.js
+++ src/historyPage/History_Display.js
@@ -15,24 +15,24 @@ class History_Display extends React.Component {
 	componentDidMount() {
 		var movHistory =[];
 		// Populate the movieHistory array in state with the movie id in the history database. 
-		firebase.firestore().collection('histories')
-		.doc('P3Fb8A9YaxaLcobOGejl')
-		.collection('history')
-		.get()
-		.then((historySnap) => {
-				historySnap.forEach((movieDoc) => {
-					firebase.firestore().collection('movies')
-					.where("id", "==", movieDoc.id)
-					.get()
-					.then(results => {
-						results.forEach(result => {
-							movHistory.push(result.data());
-						})
-					})
-				});
-				this.setState({ movieHistory: movHistory }, ()=>console.log(this.state.movieHistory));
-			}
-		); 
+		// firebase.firestore().collection('histories')
+		// .doc('P3Fb8A9YaxaLcobOGejl')
+		// .collection('history')
+		// .get()
+		// .then((historySnap) => {
+		// 		historySnap.forEach((movieDoc) => {
+		// 			firebase.firestore().collection('movies')
+		// 			.where("id", "==", movieDoc.id)
+		// 			.get()
+		// 			.then(results => {
+		// 				results.forEach(result => {
+		// 					movHistory.push(result.data());
+		// 				})
+		// 			})
+		// 		});
+		// 		this.setState({ movieHistory: movHistory }, ()=>console.log(this.state.movieHistory));
+		// 	}
+		// ); 
 	} 
 
 	render(){
