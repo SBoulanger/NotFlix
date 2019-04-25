2e62808a0888f29a378271d73de074f3711029bb
Brian Lai <brianlai@MacBook-Pro.local>
2019-04-24 20:16:55 -0700

    unified CSS

--- src/historyPage/History_Profile.js
+++ src/historyPage/History_Profile.js
@@ -3,18 +3,9 @@ import HistoryProfileLeft from './history_profile_left';
 import HistoryProfileSocialBox from './History_Profile_SocialBox';
 import HistoryProfileRight from './History_Profile_Right';
 import fire from '../libraries/Firestore';
-<<<<<<< HEAD
 import './history.css'
-=======
 import { withRouter } from 'react-router-dom';
 
-const style = {
-  display: 'flex',
-  alignItems: 'center',
-  padding: "20px",
-  background: "#eee"
-};
->>>>>>> Notflix-Signinup
 
 class HistoryProfile extends React.Component {
 
