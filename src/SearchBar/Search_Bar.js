a2de34e8a160bd1cd07e284a07b9097bad2b0470
H4RT4N <38451084+H4RT4N@users.noreply.github.com>
2019-04-30 19:29:21 -0700

    Add files via upload

--- src/SearchBar/Search_Bar.js
+++ src/SearchBar/Search_Bar.js
@@ -3,61 +3,67 @@ import './SearchBar.css';
 import fire from "../libraries/Firestore";
 import MovieSearch from "../libraries/MovieSearch";
 
-class SearchBar extends React.Component {
+// possible movie genres. currently unused array
+var movieGenres = ["Action", "Family", "Romance", "Adventure", "Fantasy", "Science Fiction", 
+                   "Animation", "History", "Thriller", "Comedy", "Horror", "TV Movie", "Crime", 
+                   "Music", "War", "Drama", "Mystery", "Western"];
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "",
-            desc: "",
-            rating: "",
-        };
+class Search_Bar extends React.Component {
+
+    constructor() {
+        super();
+        this.changeHandler = this.changeHandler.bind(this);
+    }
+
+    /*
+    	when the handler detects a space it will search for whatever is in the text field
+    	NOT the final product, just a stopgap until frontend can deliver a button function
+    */
+    changeHandler(e) {
+        var keyword = e.target.value;
+        var searchType = "title";
+        if(keyword.charAt(keyword.length - 1) == " ") {
+        	// *************** for testing *******************************************
+        	if(searchType == "genre")
+        		keyword = keyword.substring(0, keyword.length - 1);
+        	//***************remove later***************************
+            var queryRef = new MovieSearch(keyword, searchType, "", 2);
+            var getMovie = queryRef.get();
+            getMovie.then(query => {
+                if (query.empty) {
+                    // empty query = no movies found
+                    console.log("No hits found.");
+                }
+                else {
+                    query.docs.forEach(doc => 
+                    {
+                        if(doc.exists) {
+                            // show movie data
+                            console.log("Document data:", doc.data());
+                            console.log("Found a hit.");
+                        }
+                        else {
+                            //do nothing
+                            console.log("Document doesn't exist.");
+                        }
+                    })
+                }
+            }); 
+        }
     }
 
     render() {
         return (
             <div className="ui-header">
-            
-            </div>
-        )
-    }
-
-    render_wip() {
-        return (
-            <div className="ui-header">
-
-                <input href="#search" className="search-bar" type="Text" placeholder="Search Movies"></input>
-
+                <input 
+                    className="search-bar" 
+                    type="Text" 
+                    onChange={this.changeHandler}
+                    placeholder="Enter the 1st word of a title and a space to search."
+                />
             </div>
         )
     }
 }
 
-/* 
-    testing the MovieSearch library
-    everything is on console at the moment.
-
-*/
-var queryRef = new MovieSearch("John", "title", "", 5);
-var getMovie = queryRef.get().then(query => {
-        if (query.empty) {
-            // empty query = no movies found
-            console.log("No hits found.");
-        }
-        else {
-            query.docs.forEach(doc => {
-                    if(doc.exists){
-                        // show movie data
-                        console.log('Document data:', doc.data());
-                        console.log("Found a hit.");
-
-                    }
-                    else{
-                        //do nothing
-                        console.log("document doesn't exist");
-                    }
-            })
-        }
-}); 
-
-export default SearchBar;
+export default Search_Bar;
