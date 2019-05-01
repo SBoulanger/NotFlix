33ce6c558602bb946d90e2eb0e04c406ef462b22
Brian Lai <brianlai@MacBook-Pro-6.local>
2019-04-30 22:51:24 -0700

    history fetch working

--- src/historyPage/History_Display.js
+++ src/historyPage/History_Display.js
@@ -11,59 +11,16 @@ class HistoryDisplay extends React.Component {
 		this.state = {
 			movieHistory: []
 		};
-
-		// For Testing CSS
-		// this.state = {
-		// 			movieHistory: [
-		// 				{title: "Movie1", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"}, 
-		// 				{title: "Movie2", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"}, 
-		// 				{title: "Movie3", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"}
-		// 			]
-		// 		};
 	}
 
-	// NOT WORKING 
 	componentDidMount() {
-		// Populate the movieHistory array in state with the movie id in the history database. 
 
-		// const getMovies = (id) => {
-		//     return new Promise((resolve, reject) => {
-		//         let movs = [];
-		//         const snapshot = firebase.db.collection('histories')
-		//             .doc(id)
-		//             .get()
-		//             .then((snapshot) => {
-		//                 snapshot.data().Movies.forEach(mov => {
-		//                 	firebase.db.collection('movies')
-  //      						.doc(mov)
-  //      						.get()
-  //      						.then(doc => {
-  //      							movs.push(doc.data());
-  //      					});
-		//                 });
-		//                 resolve(movs); // return the movies only after they are fetched
-		//             	console.log(movs);
-		//             })
-		//             .catch(error => {
-		//                 reject(error);
-		//             });
-		//     });
-		// };
-
-		// let movies = [];
-		// getMovies(Cookie.get()).then(movs => {
-		// 	movies = movs;
-		// 	this.setState({movieHistory: movies}, () => {this.setState({movieHistory: movies})});
-		// });
-
-
-		var movIds = [];
 		var movies = [];
 		firebase.db.collection('histories')
 			.doc(Cookie.get())
 			.get()
 			.then(doc => {
-				movIds = doc.data().Movies.map(movie => {
+				movies = doc.data().Movies.map(movie => {
 					return firebase.db.collection('movies')
 						.doc(movie)
 						.get()
@@ -72,7 +29,7 @@ class HistoryDisplay extends React.Component {
 						});
 					
 				})
-				Promise.all(movIds).then(values => {
+				Promise.all(movies).then(values => {
 					this.setState({movieHistory: values});
 					console.log(this.state.movieHistory);
 				})
@@ -83,7 +40,7 @@ class HistoryDisplay extends React.Component {
 	render(){
 
 		let historyMovies = this.state.movieHistory.map(movie => {
-			return <HistoryMovie title={movie.title} overview={movie.overview} />
+			return <HistoryMovie title={movie.title} overview={(movie.overview.length > 260 && movie.title.length > 30) ? (movie.overview.substring(0, 260) + "...") : movie.overview} />
 		});
 		return(
 
