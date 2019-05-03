ded5927619ac30dc0b7cad74301ad38de0d6f09a
H4RT4N <38451084+H4RT4N@users.noreply.github.com>
2019-05-02 20:56:33 -0700

    Add files via upload

--- src/SearchBar/Search_Bar.js
+++ src/SearchBar/Search_Bar.js
@@ -3,33 +3,20 @@ import './SearchBar.css';
 import fire from "../libraries/Firestore";
 import MovieSearch from "../libraries/MovieSearch";
 
-// possible movie genres. currently unused array
-var movieGenres = ["Action", "Family", "Romance", "Adventure", "Fantasy", "Science Fiction", 
-                   "Animation", "History", "Thriller", "Comedy", "Horror", "TV Movie", "Crime", 
-                   "Music", "War", "Drama", "Mystery", "Western"];
+var keyword = "";
 
 class Search_Bar extends React.Component {
 
     constructor() {
         super();
         this.changeHandler = this.changeHandler.bind(this);
+        this.buttonClicked = this.buttonClicked.bind(this);
     }
 
-    /*
-    	when the handler detects a space it will search for whatever is in the text field
-    	NOT the final product, just a stopgap until frontend can deliver a button function
-    */
-    changeHandler(e) {
-        var keyword = e.target.value;
-        var searchType = "title";
-        if(keyword.charAt(keyword.length - 1) == " ") {
-        	// *************** for testing *******************************************
-        	if(searchType == "genre")
-        		keyword = keyword.substring(0, keyword.length - 1);
-        	//***************remove later***************************
-            var queryRef = new MovieSearch(keyword, searchType, "", 2);
-            var getMovie = queryRef.get();
-            getMovie.then(query => {
+    buttonClicked(e) {
+        if(keyword != "") {
+            var queryRef = new MovieSearch(keyword, "title", "", 2);
+            var getMovie = queryRef.get().then(query => {
                 if (query.empty) {
                     // empty query = no movies found
                     console.log("No hits found.");
@@ -39,28 +26,38 @@ class Search_Bar extends React.Component {
                     {
                         if(doc.exists) {
                             // show movie data
-                            console.log("Document data:", doc.data());
+                            console.log('Document data:', doc.data());
                             console.log("Found a hit.");
                         }
                         else {
                             //do nothing
-                            console.log("Document doesn't exist.");
+                            console.log("document doesn't exist");
                         }
                     })
                 }
-            }); 
+            });
         }
+    }
+
+    changeHandler(e) {
+        keyword = e.target.value;
     }
 
     render() {
         return (
             <div className="ui-header">
                 <input 
+                    href="#search" 
                     className="search-bar" 
                     type="Text" 
                     onChange={this.changeHandler}
-                    placeholder="Enter the 1st word of a title and a space to search."
+                    placeholder="Search..."
                 />
+                <button 
+                    href="#search"
+                    onClick={this.buttonClicked}
+                    className="search_button">Search</button>
+                
             </div>
         )
     }
